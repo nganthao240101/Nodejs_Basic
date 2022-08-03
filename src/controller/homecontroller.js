183a9e604ef127f9043c393123ad7cab3fdf404c
NGAN <NGAN@DESKTOP-M5ENFJV>
2022-08-03 23:44:31 +0700

    video11

---
+++ src/controller/homecontroller.js
@@ -0,0 +1,23 @@
+
+import connection from '../configs/connectDB';
+let getHomePage = (req, res) => {
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users`',
+        function(err, results, fields) {
+          results.map((row)=> {data.push({
+            id:row.id,
+            firstName:row.firstName,
+            lastName:row.lastName,
+            email:row.email,
+            address:row.address
+            })
+        });
+          return res.render('test/index.ejs',{dataUser:JSON.stringify(data)})
+    });
+    
+     
+}
+
+module.exports={getHomePage
+}
